5150cec84f9ff1451cb2d23152cde9affd3b37cb
roaraujo1 <61365764+roaraujo1@users.noreply.github.com>
2023-09-15 17:46:46 -0700

    Time: 68 ms (47.58%), Space: 16.5 MB (90.30%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = {}
+        for i in magazine:
+            count[i] = 1+count.get(i,0)
+        
         for i in ransomNote:
-            if ransomNote.count(i) <= magazine.count(i):
-                continue
+            if i in count and count[i]>0:
+                count[i]-=1
             else:
                 return False
         return True
+            
