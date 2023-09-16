aaee0c112c88532bf8bd1398c361bc1631b4132b
roaraujo1 <61365764+roaraujo1@users.noreply.github.com>
2023-09-15 17:42:42 -0700

    Time: 177 ms (5.02%), Space: 16.5 MB (61.10%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if ransomNote.count(i) <= magazine.count(i):
+                continue
+            else:
+                return False
+        return True
